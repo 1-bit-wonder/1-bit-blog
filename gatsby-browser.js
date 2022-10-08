b299954d6596b630b3a8a2af145fd3934c74df02
Nigel <nigel@astrangequark.xyz>
2022-10-07 23:19:53 -0500

    initial commit

---
+++ gatsby-browser.js
@@ -0,0 +1,12 @@
+// custom typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+// web fonts
+import "@fontsource/source-sans-pro"
+import "@fontsource/spectral"
+// Highlighting for code blocks
+import "prismjs/themes/prism-okaidia.css"
